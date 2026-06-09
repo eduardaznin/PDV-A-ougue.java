028ecd1742eff9fdf8dd362ffb1117849404de44
eduardaznin <eduardazaninabreu@gmail.com>
2026-06-08 21:23:25 -0300

    Update Funcionario.java

--- src/classes/Funcionario.java
+++ src/classes/Funcionario.java
@@ -1,1 +1,24 @@
 
+public class Funcionario extends Pessoa {
+
+    private String senha;
+
+    public Funcionario(String nome, String cpf, String telefone, String senha){
+        super(nome, cpf, telefone);
+        this.senha = senha;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    @Override
+    public String exibir() {
+        return "Funcionario{" + "nome=" + getNome() + "cpf=" + getCpf() + "telefone=" + getTelefone() + "}";
+    }
+    
+}
